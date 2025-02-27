171a5169e1b819f832e4df06029f94a1f760652e
amulyasb <amul.baidhya123@gmail.com>
2025-02-27 22:33:51 +0545

    fetching rooms in homepage frontend(featured room section)

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from accounts.models import *
+from rooms.models import *
 
 # Create your views here.
 
@@ -7,17 +8,23 @@ def homepage(request):
     how_it_work_section = request.GET.get('type', 'customerSection')  # Default to 'customerSection'
     cities = city.objects.all()
     user = None
+    featured_rooms = Room.objects.all().order_by("-id")[:4]
+
 
     if request.user.is_authenticated:
         user = request.user
         # Check if the user is not a customer
         if request.user.user_type != 'customer':  
             return redirect('login_user')
+        
+        featured_rooms = Room.objects.all().order_by("-id")[:4]
+
 
     data = {
         'type': how_it_work_section,
         'user': user,
         'cities': cities,
+        'featured_rooms': featured_rooms,
     }
     return render(request, "core/homepage.html", data)
 
